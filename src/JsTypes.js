a1a6f2e1cd5f6f17e5cd5cc7d99d95e0e8b282eb
amj311 <amjudd315@gmail.com>
2020-12-23 12:09:55 -0700

    improved type recognition

--- src/JsTypes.js
+++ src/JsTypes.js
@@ -1,33 +1,24 @@
 const typeNames = new Map(); // Map<Type,String>
+var util = require('util');
 
+typeNames.set(String,"string");
+typeNames.set(Array,"Array");
+typeNames.set(Promise,"Promise");
 
 determineType = function(obj) {
-    let type;
-    Array.from(typeNames.keys()).forEach(key => {
-        if (obj instanceof key) type = typeNames.get(key);
-    })
-    return type? type : typeof obj;
+    let type = typeof obj;
+    if (type == "object") {
+        Array.from(typeNames.keys()).forEach(key => {
+            if (obj instanceof key) type = typeNames.get(key);
+        })
+    }
+    if (type == "object") {
+        if (obj.__proto__) {
+            let str = util.format(obj.__proto__);
+            type = str.substr(0,str.indexOf(" {"))
+        }
+    }
+    return type;
 }
-
-
-typeNames.set(String,"String");
-typeNames.set(Array,"Array");
-typeNames.set(Date,"Date");
-typeNames.set(Map,"Map");
-typeNames.set(WeakMap,"WeakMap");
-typeNames.set(Set,"Set");
-typeNames.set(WeakSet,"WeakSet");
-typeNames.set(Promise,"Promise");
-typeNames.set(Function,"Function");
-typeNames.set(ArrayBuffer,"ArrayBuffer");
-typeNames.set(SharedArrayBuffer,"SharedArrayBuffer");
-typeNames.set(BigInt64Array,"BigInt64Array");
-typeNames.set(BigUint64Array,"BigUint64Array");
-typeNames.set(Float32Array,"Float32Array");
-typeNames.set(Float64Array,"Float64Array");
-typeNames.set(Int16Array,"Int16Array");
-typeNames.set(Int32Array,"Int32Array");
-typeNames.set(Int8Array,"Int8Array");
-typeNames.set(DataView,"DataView");
 
 module.exports = {determineType, typeNames}
